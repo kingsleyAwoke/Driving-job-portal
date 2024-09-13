21af4324426a67b8359c3593bfc0d32601f027a7
kingsley Awoke <kingsley_social@yahoo.com>
2024-09-13 18:46:41 +0100

    Modified to add security system.

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const pool = require('./db');
-//const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const { Pool } = require('pg');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
@@ -13,6 +12,16 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// DATABASE CONNECTION
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
 
 // Helper function to send OTP
 const sendOtp = async (email, mobile_number, otp) => {
